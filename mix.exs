8c83a2ae4d76f6d3d4f10d0f100dce902f202420
blazpie <blazej.pierzak@gmail.com>
2023-01-23 13:07:16 +0000

    bump ver

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTSP.MixProject do
   use Mix.Project
 
-  @version "0.3.0"
+  @version "0.3.1"
   @github_url "https://github.com/membraneframework/membrane_rtsp"
 
   def project do
