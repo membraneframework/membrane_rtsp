db1b6075ebe8ad6ec2f62aa716cb887b5a8580b9
noarkhh <jakub.pryc@gmail.com>
2024-08-23 10:38:35 +0200

    Change from minor to patch bump

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTSP.MixProject do
   use Mix.Project
 
-  @version "0.8.0"
+  @version "0.7.2"
   @github_url "https://github.com/membraneframework/membrane_rtsp"
 
   def project do
